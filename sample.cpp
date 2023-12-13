cf5c939ac07a9262861710a373a856ad88b90543
suryapeddireddy <suryapeddireddy1203@gmail.com>
2023-12-13 20:19:41 +0530

    Added Pow
    This takes two very large integers and gives pow(a, b)

--- sample.cpp
+++ sample.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+int mod=1e9+7;
+long long Pow(long long a, long long b) {
+    long long result = 1;
+
+    // Update a to be in the range [0, m) to handle negative values of a
+    a = a % mod;
+    while (b > 0) {
+        // If b is odd, multiply the result with a
+        if (b % 2 == 1) {
+            result = (result * a) % mod;
+        }
+        // Square a and reduce it modulo m
+        a = (a * a) % mod;
+
+        // Divide b by 2
+        b /= 2;
+    }
+
+    return result;
+}
 
 int main(){
 int n; cin>>n;
