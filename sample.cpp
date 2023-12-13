26d8e9f06b8d4c3f4cb6a54d9555ec38da1036e5
suryapeddireddy <suryapeddireddy1203@gmail.com>
2023-12-13 20:04:15 +0530

    Create sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n; cin>>n;
+cout<<"congratulations You have made Your first sample repository"<<endl;
+return 0;
+}
